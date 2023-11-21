c39be0a1df6a9c3b6abe839f8d76ca88eecb9f31
SinghAmandeep <amandeepsingh3095@gmail.com>
2023-11-21 15:42:32 +0100

    add-form-file/done-milestone

---
+++ form.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Badwords</title>
+</head>
+<body>
+
+
+<h1>Bad Words</h1>
+<form action="index.php" method='GET'>
+    <input type="text" placeholder='inserici il testo' name='user_text'>
+    <button type='submit'>Invia</button>
+</form>
+</body>
+</html>
